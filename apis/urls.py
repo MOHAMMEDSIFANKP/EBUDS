ed821feacbb12b4ab46348256f75fbbc0b3881d1
sifan007sifu@gmail.com <sifan007sifu@gmail.com>
2023-06-17 01:27:57 +0530

    api add product function

--- apis/urls.py
+++ apis/urls.py
@@ -2,6 +2,7 @@
 from django.urls import include, path
 # import routers
 from rest_framework import routers
+from .import views
 
 # import everything from views
 from .views import *
@@ -15,5 +16,6 @@ router.register(r'geeks', GeeksViewSet)
 # specify URL Path for rest_framework
 urlpatterns = [
 	path('', include(router.urls)),
+    path('products/<int:prod_id>/',views.products, name="products"),
 	path('api-auth/', include('rest_framework.urls'))
 ]
--- apis/views.py
+++ apis/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
 # import viewsets
 from rest_framework import viewsets
+from django.core.serializers import serialize
+from django.forms.models import model_to_dict
+
 
 # import local data
 from .serializers import GeeksSerializer
@@ -15,3 +18,20 @@ class GeeksViewSet(viewsets.ModelViewSet):
 	
 	# specify serializer to be used
 	serializer_class = GeeksSerializer
+
+def products(request, prod_id):
+    try:
+        prod = GeeksModel.objects.get(id=prod_id)
+        prod_dict = {
+            "id": prod.id,
+            "price": prod.price,
+            "description": prod.description,
+            "photo": prod.photo.url if prod.photo else None,
+        }
+        response = JsonResponse(prod_dict)
+        response['Content-Type'] = 'application/json'
+        return response
+    except GeeksModel.DoesNotExist:
+        response = JsonResponse({"error": "Product not found"})
+        response.status_code = 404
+        return response
