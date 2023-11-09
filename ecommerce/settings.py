4b32358637ff8aed5a082728b650508278a9d4d8
sifan <sifan007sifu@gmail.com>
2023-11-09 23:12:04 +0530

    Collect static updated

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -141,9 +141,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS= [
-    os.path.join(BASE_DIR / 'staticfiles')
+    os.path.join(BASE_DIR / 'static')
 ]
 
 # Default primary key field type
