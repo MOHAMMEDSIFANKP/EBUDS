3cd1b434f97dc93809f81c4293b3b5d0eecc94c9
sifan <sifan007sifu@gmail.com>
2024-03-27 10:38:52 +0530

    setting changed db

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -95,17 +95,22 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('dbname'),                      
+#         'USER': config('dbuser'),
+#         'PASSWORD': config('dbpass'),
+#         'HOST': config('dbhost'),
+#         'PORT': '5432',
+#     }
+# }   
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('dbname'),                      
-        'USER': config('dbuser'),
-        'PASSWORD': config('dbpass'),
-        'HOST': config('dbhost'),
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-}   
-
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
